afa3e9f61a43fa955a020f1659eba56702fd7e39
Kleber <kleber.meira@redetendencia.com.br>
2020-12-30 09:19:22 -0400

    finalizando crud usado ORM Sequelize

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,12 +1,17 @@
 const { Router } = require('express')
 const PessoaController = require('../controllers/PessoaController')
+const { route } = require('./niveisRoute')
 
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
 router.post('/pessoas', PessoaController.criaPessoa)
+router.post('pessoas/:estudanteId/matricula/', PessoaController.criaMatricula)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
 router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+
 
 module.exports = router
